29a435454b4e3a184bd129ee3a0bc2347e988df4
sajeethan19 <73322508+sajeethan19@users.noreply.github.com>
2022-10-11 09:20:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function(){
+        
+        
+            
+        
+    }
+);
+
+
+var w = window.innerWidth;
+anime({
+        targets: '.brandlogo',
+        translateX: w*0.35,
+        duration: 5000,
+    },
+    );
